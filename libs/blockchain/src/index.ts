81b10a4105953e26b74515ca238b324deaee459f
Mikhail Andreev <tundraray@outlook.com>
2026-01-22 01:24:23 +0200

    feat(blockchain): add transaction event name constants
    - Add TRANSACTION_NEW_EVENT for new transaction detection
    - Add TRANSACTION_CONFIRMED_EVENT for future use
    - Add TransactionEvents object for type-safe event access
    - Export events from blockchain library index
    
    Implements AC-5.1 event name specification.

--- libs/blockchain/src/index.ts
+++ libs/blockchain/src/index.ts
@@ -1,4 +1,5 @@
 export * from './blockchain.module';
 export * from './blockchain.service';
+export * from './events/transaction.events';
 export * from './interfaces/transaction.interface';
 export * from './services/deduplication.service';
