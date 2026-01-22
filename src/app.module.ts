5db921f49f2ecd8d6e1b06bf14abbf97677225e6
Mikhail Andreev <tundraray@outlook.com>
2026-01-22 12:08:33 +0200

    feat(app): wire DbModule and BlockchainModule in AppModule
    Connect database and blockchain modules to main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
+import { BlockchainModule } from '@app/blockchain';
+import { DbModule } from '@app/db';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [DbModule, BlockchainModule],
 })
 export class AppModule {}
